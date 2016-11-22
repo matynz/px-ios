1962bd8599fa1cb7550a682be07893e8c184bebe
edentorres <eden.etorres@gmail.com>
2016-11-22 13:32:57 -0300

    F3

---
+++ js/snippets.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+//$( "#flavor_3_step_1" ).append( "source 'https://github.com/CocoaPods/Specs.git' \n# Se necesita este parámetro por ser una SDK en swiftuse_frameworks!\nplatform :ios, '8.0'\n pod 'MercadoPagoSDK', '2.0.8'" );
+//$( "#flavor_3_step_1" ).append( "hola" );
+var container = document.getElementById("flavor_3_step_1");
+//container.innerHTML = "<script src=\"https://gist.github.com/edentorres/765825e1775ce8e971e813abae2badc0.js?file=pod\"></script>";
+//$( "#flavor_3_step_1" ).append( "<script data-cfasync=false src=\"https://gist.github.com/edentorres/765825e1775ce8e971e813abae2badc0.js?file=pod\"></script>" );
+})
+
